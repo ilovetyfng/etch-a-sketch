37c23d4234fa740bed133827c93421ed63c5e213
ilovetyfng <enderkiller3472@gmail.com>
2022-02-07 17:29:40 +0800

    Add change color when hover

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+function feSlave (e) {
+    e.addEventListener('mouseover', changeColor);
+}
+
+function changeColor (e){
+    e.target.classList.add('hovering');
+}
+
 const container = document.querySelector('#container');
 const row = document.createElement('div');
 const square = document.createElement('div');
 
 row.setAttribute('id', 'row');
-square.setAttribute('id', 'square');
+square.setAttribute('class', 'square');
 
 for (i = 0; i < 16; i++) {
     row.appendChild(square.cloneNode(true));
@@ -12,3 +20,8 @@ for (i = 0; i < 16; i++) {
 for (i = 0; i < 16; i++) {
     container.appendChild(row.cloneNode(true));
 }
+
+
+
+console.log(document.querySelectorAll('.square'))
+document.querySelectorAll('.square').forEach(feSlave);
